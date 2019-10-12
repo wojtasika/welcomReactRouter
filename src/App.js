fb9112c7fc57f1b8055c2e6f5d7eab1031bea7af
AgaWojtasik <agawojtasik25@wp.pl>
2019-10-12 23:56:42 +0200

    Menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
+
+
+const Home = () => <h1>Strona startowa</h1>
+const News = () => <h1>Aktualności</h1>
+const Contact = () => <h1>Kontakt do nas</h1>
+
+const ErrorPage = () => <h1>Strona nie istnieje</h1>
 
 class App extends Component {
   render() {
@@ -14,16 +21,24 @@ class App extends Component {
                 <li><a href="/news">Aktualności</a></li>
                 <li><a href="/contact">Kontakt</a></li> */}
 
-                <li><Link to="/">Start</Link></li>
-                <li><Link to="/news">Aktualności</Link></li>
-                <li><Link to="/contact">Kontakt</Link></li>
+                <li><NavLink to="/" exact activeClassName="home_selected" activeStyle={{
+                  backgroundColor: 'grey',
+                  letterSpacing: '6px'
+                }}>Start</NavLink></li>
+                <li><NavLink to="/news" activeClassName="news">Aktualności</NavLink></li>
+                <li><NavLink to="/contact" activeClassName="contact">Kontakt</NavLink></li>
 
               </ul>
             </nav>
           </header>
           <section>
-            Strona - Witaj!
-        </section>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/news" component={News} />
+              <Route path="/contact" component={Contact} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </section>
         </div>
       </BrowserRouter>
     );
